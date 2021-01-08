ada4ff2aa6962209ccabed361178a5e8b5219f13
TBBdlz <metee.yingyong@kkumail.com>
2021-01-08 10:02:19 +0700

    add lab2

---
+++ lab2/metee/yingyongwatthanakit/Patient.java
@@ -0,0 +1,24 @@
+/*
+  This Patient program is to accept three arguments: patient name, patient age
+  and country.  Its output format is
+  Patient's name is <patient name>
+  Patient's age is <patient age>
+  <patient name> comes from  <patient country>
+  Author: Metee Yingyongwatthanakit
+  ID: 633040174-2
+  Sec: 2
+  Date: 8 January 2021
+*/
+package lab2.metee.yingyongwatthanakit;
+
+public class Patient {
+    public static void main(String[] args) {
+        if(args.length == 3) {
+            System.out.println("Patient's name is " + args[0]);
+            System.out.println("Patient's age is " + args[1]);
+            System.out.println(args[0] + " comes from " + args[2]);
+        }else {
+            System.err.println("Patient <patient name> <patient age> <country>");
+        }
+    }
+}
