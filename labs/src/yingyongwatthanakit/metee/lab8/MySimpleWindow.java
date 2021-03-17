ca7185a42ca1b22c7c9d25a8d40c2a13838c0762
TBBdlz <metee.yingyong@kkumail.com>
2021-03-17 23:44:08 +0700

    add to-be-done programs

---
+++ labs/src/yingyongwatthanakit/metee/lab8/MySimpleWindow.java
@@ -0,0 +1,51 @@
+package yingyongwatthanakit.metee.lab8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MySimpleWindow extends JFrame {
+
+     protected JPanel mainPanel, buttonPanel;
+     protected JButton resetButton, submitButton;
+
+    public MySimpleWindow(String title) {
+        super(title);
+    }
+
+    protected void initComponents() {
+        mainPanel = (JPanel) this.getContentPane();
+        buttonPanel = new JPanel();
+        resetButton = new JButton("reset");
+        submitButton = new JButton("submit");
+    }
+
+     protected void addComponents() {
+         buttonPanel.add(resetButton);
+         buttonPanel.add(submitButton);
+         mainPanel.add(buttonPanel);
+     }
+
+     protected void setFrameFeatures() {
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     }
+
+     public static void createAndShowGUI() {
+        MySimpleWindow msw = new MySimpleWindow("My Simple Window");
+        msw.initComponents();
+        msw.addComponents();
+        msw.setFrameFeatures();
+     }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+}
---
+++ labs/src/yingyongwatthanakit/metee/lab8/PlayerFormV1.java
@@ -0,0 +1,77 @@
+package yingyongwatthanakit.metee.lab8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerFormV1 extends MySimpleWindow {
+
+    protected JPanel formPanel, genderPanel;
+    protected JLabel nameLabel, nationalityLabel, dobLabel, genderLabel;
+    protected JTextField nameText, nationalityText, dobText;
+    protected JRadioButton maleRadioButton, femaleRatioButton, othersRatioButton;
+    protected ButtonGroup genderGrp;
+
+    public PlayerFormV1(String title) {
+        super(title);
+    }
+
+    @Override
+    protected void initComponents() {
+        super.initComponents();
+        formPanel = new JPanel();
+        genderPanel = new JPanel();
+        nameLabel = new JLabel("Name:");
+        nationalityLabel = new JLabel("Nationality:");
+        dobLabel = new JLabel("Date of Birth (eg.,31-01-1990):");
+        genderLabel = new JLabel("Gender:");
+        nameText = new JTextField();
+        nationalityText = new JTextField();
+        dobText = new JTextField();
+        maleRadioButton = new JRadioButton("Male", true);
+        femaleRatioButton = new JRadioButton("Female");
+        othersRatioButton = new JRadioButton("Others");
+        genderGrp = new ButtonGroup();
+    }
+
+    @Override
+    protected void addComponents() {
+        super.addComponents();
+        mainPanel.setLayout(new BorderLayout());
+        // Gender Panel Add Components
+        genderGrp.add(maleRadioButton);
+        genderGrp.add(femaleRatioButton);
+        genderGrp.add(othersRatioButton);
+        genderPanel.add(maleRadioButton);
+        genderPanel.add(femaleRatioButton);
+        genderPanel.add(othersRatioButton);
+
+        formPanel.setLayout(new GridLayout(4, 2));
+        formPanel.add(nameLabel);
+        formPanel.add(nameText);
+        formPanel.add(nationalityLabel);
+        formPanel.add(nationalityText);
+        formPanel.add(dobLabel);
+        formPanel.add(dobText);
+        formPanel.add(genderLabel);
+        formPanel.add(genderPanel);
+        mainPanel.add(formPanel, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public static void createAndShowGUI() {
+        PlayerFormV1 msw = new PlayerFormV1("Player Form V1");
+        msw.initComponents();
+        msw.addComponents();
+        msw.setFrameFeatures();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+}
